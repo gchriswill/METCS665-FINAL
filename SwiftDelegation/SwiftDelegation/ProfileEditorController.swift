f610ddcc8e856549b3ec8ea08a4dac7f24d2b1b8
gchriswill <4576678+gchriswill@users.noreply.github.com>
2021-04-29 00:26:46 -0500

    Adds code documentation for the `ProfileEditorController` class

--- SwiftDelegation/SwiftDelegation/ProfileEditorController.swift
+++ SwiftDelegation/SwiftDelegation/ProfileEditorController.swift
@@ -7,29 +7,40 @@
 
 import UIKit
 
+/**
+ A class controller for managing the view and data of the create/edit screen.
+ This class is fully responsible for the creationg and edition of the data and the views updates
+ */
 class ProfileEditorController: UIViewController {
     
+    // Binder Outlets for linking the accessed views to the controller
+    // Basically this properties are the association part of the views to the controller
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var profileEditActionButton: UIButton!
     
+    // The binder action from the button view that triggers on user interaction and executes the create/update
+    // based on the configuration of the controller
     @IBAction func profileEditAction(_ sender: UIButton) {
         
-        // Potential usecase for a Delegation pattern for the behavior of cell configuration with Profile class
+        // Potential usecase for a Delegation pattern
         guard ProfileEditorController.verifyFormatFor(username: userNameTextField.text ?? "") else {
             return
         }
         
+        // Potential usecase for a Delegation pattern
         guard ProfileEditorController.verifyFormatFor(name: nameTextField.text ?? "") else {
             return
         }
         
+        // Potential usecase for a Delegation pattern
         guard ProfileEditorController.verifyFormatFor(lastName: lastNameTextField.text ?? "") else {
             return
         }
         
+        // Potential usecase for a Delegation pattern
         guard ProfileEditorController.verifyFormatFor(email: emailTextField.text ?? "") else {
             return
         }
@@ -73,6 +84,7 @@ class ProfileEditorController: UIViewController {
         }
     }
     
+    // The binder action that triggers on user interaction for canceling the create/update
     @IBAction func profileCancelAction(_ sender: UIButton) {
         
         if configuration == .create {
@@ -85,7 +97,7 @@ class ProfileEditorController: UIViewController {
     }
     
 //    var validatorDelegate: ProfileDataVerificationDelegate?
-    
+    // An enum for specifying the configuration of the controller
     enum EditorConfiguration: String {
         case create = "edu.bu.chriswgm.UIStoryboardSegue.create",
              update = "edu.bu.chriswgm.UIStoryboardSegue.update"
@@ -101,15 +113,19 @@ class ProfileEditorController: UIViewController {
         }
     }
     
+    // The property for holding the configuration value of the controller
     var configuration: EditorConfiguration?
     
+    // The profile data object for temporary holding
     var profile: Profile?
     
+    // The constructor of the controller
     required init?(coder: NSCoder) {
         super.init(coder: coder)
 //        validatorDelegate = ProfileDataValidator()
     }
     
+    // The function to load and configure the view and the data for the given configuration
     func loadEditorConfiguration(configuration: EditorConfiguration) {
         
         let titleName = EditorConfiguration.titleName(configuration: configuration)
@@ -130,6 +146,7 @@ class ProfileEditorController: UIViewController {
         }
     }
     
+    // Default native Life cycle method
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -145,11 +162,10 @@ class ProfileEditorController: UIViewController {
         loadEditorConfiguration(configuration: config)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-    }
-    
-    // Potential usecase for a Delegation pattern for the behavior of cell configuration with Profile class
+    /**
+     The following functions are for data validation based on rules that where gathered from the assumptions list
+     */
+    // Potential usecase for a Delegation pattern
     static func verifyStandartFormat(verstr: String) -> Bool {
         
         let cleanValue = verstr.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -169,7 +185,7 @@ class ProfileEditorController: UIViewController {
         return true
     }
     
-    // Potential usecase for a Delegation pattern for the behavior of cell configuration with Profile class
+    // Potential usecase for a Delegation pattern
     static func verifyFormatFor(username: String) -> Bool {
         
         guard verifyStandartFormat(verstr: username) else {
@@ -191,7 +207,7 @@ class ProfileEditorController: UIViewController {
         return true
     }
     
-    // Potential usecase for a Delegation pattern for the behavior of cell configuration with Profile class
+    // Potential usecase for a Delegation pattern
     static func verifyFormatFor(name: String) -> Bool {
         
         guard verifyStandartFormat(verstr: name) else {
@@ -205,7 +221,7 @@ class ProfileEditorController: UIViewController {
         return true
     }
     
-    // Potential usecase for a Delegation pattern for the behavior of cell configuration with Profile class
+    // Potential usecase for a Delegation pattern
     static func verifyFormatFor(lastName: String) -> Bool {
         
         guard verifyStandartFormat(verstr: lastName) else {
@@ -219,7 +235,7 @@ class ProfileEditorController: UIViewController {
         return true
     }
     
-    // Potential usecase for a Delegation pattern for the behavior of cell configuration with Profile class
+    // Potential usecase for a Delegation pattern
     static func verifyFormatFor(email: String) -> Bool {
         
         guard verifyStandartFormat(verstr: email) else {
